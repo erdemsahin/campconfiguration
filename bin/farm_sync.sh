fdc3d63904ee8776b53d6946c82ec0b6c7f80866
Erdem Uslu <erdem.uslu@vlmedia.com.tr>
2024-01-30 15:31:25 +0300

    Update farm_sync script

---
+++ bin/farm_sync.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+BASE_CONF_DIR=/opt/camp/
+
+cd /tmp/ && git clone https://github.com/erdemsahin/campconfiguration.git \
+    && cd /tmp/campconfiguration && git checkout main
+
+mkdir -p ${BASE_CONF_DIR} || echo "Not created config directory"; exit 1
+
+rsync -aP /tmp/campconfiguration --exclude=".git" ${BASE_CONF_DIR}
